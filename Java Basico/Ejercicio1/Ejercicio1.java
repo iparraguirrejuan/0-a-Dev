bc87f1aeecc332c9e3cc0d2e0e2e7f9ed948aff8
iparraguirrejuan <juan.ciparraguirre@gmail.com>
2022-10-26 21:19:26 -0300

    Ejercicio 1

---
+++ Java Basico/Ejercicio1/Ejercicio1.java
@@ -0,0 +1,19 @@
+package Ejercicio1;
+public class Ejercicio1{
+    public static void main(String[] args) {
+        String texto = "Hola Mundo!";
+        int numero = 13;
+        Double decimal = 13.34d;
+        long longitud = 67574854L;
+        boolean verdadero = true;
+
+        System.out.println("Dato de tipo String: " + texto);
+        System.out.println("Dato de tipo int: " + numero);
+        System.out.println("Dato de tipo Double: " + decimal);
+        System.out.println("Dato de tipo long: " + longitud);
+        System.out.println("Dato de tipo booleano: " + verdadero);
+
+        
+    }
+
+}
