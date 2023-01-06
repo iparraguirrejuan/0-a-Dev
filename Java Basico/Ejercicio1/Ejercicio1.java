7753a0ee0dd4d8bd5e1db2efc71997335fabf407
iparraguirrejuan <juan.ciparraguirre@gmail.com>
2023-01-05 21:15:17 -0300

    Ejercicio 1 - completado

--- Java Basico/Ejercicio1/Ejercicio1.java
+++ Java Basico/Ejercicio1/Ejercicio1.java
@@ -6,12 +6,18 @@ public class Ejercicio1{
         Double decimal = 13.34d;
         long longitud = 67574854L;
         boolean verdadero = true;
+        boolean falso = false;
+        float decimal1 = 5.6f;
+        char texto1 = 'a';
 
         System.out.println("Dato de tipo String: " + texto);
         System.out.println("Dato de tipo int: " + numero);
         System.out.println("Dato de tipo Double: " + decimal);
         System.out.println("Dato de tipo long: " + longitud);
         System.out.println("Dato de tipo booleano: " + verdadero);
+        System.out.println("Dato de tipo booleano" + falso);
+        System.out.println("Dato de tipo numero - float: "+decimal1);
+        System.out.println("Dato de tipo texto - char: "+ texto1);
 
         
     }
